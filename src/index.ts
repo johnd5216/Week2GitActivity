1ba052651da7a7ab9a6dd20e7d09ea1779146bff
johnd5216 <johnd5216@gmail.com>
2024-09-15 16:12:32 -0400

    print done

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,4 @@ const result_multiply = calc.multiply(5, 3);
 console.log(`result_multiply = ${result_multiply}`);
 const result_divide = calc.divide(6, 3);
 console.log(`result_divide = ${result_divide}`);
+console.log('done.')
