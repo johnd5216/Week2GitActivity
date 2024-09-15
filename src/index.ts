75add3097369a7bf016acb5ff92777c2c760a753
johnd5216 <johnd5216@gmail.com>
2024-09-15 16:01:44 -0400

    multiply operation

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,5 @@ const result_add = calc.add(2, 3);
 console.log(`result_add = ${result_add}`);
 const result_subtract = calc.subtract(5, 3);
 console.log(`result_subtract = ${result_subtract}`);
+const result_multiply = calc.multiply(5, 3);
+console.log(`result_multiply = ${result_multiply}`);
